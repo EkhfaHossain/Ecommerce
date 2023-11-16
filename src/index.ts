c33e2bcbdec94adb1326d61ac789454f130213e2
EkhfaHossain <ekhfa.hossain@sazim.io>
2023-11-16 21:10:53 +0600

    Added Error Handling for each endpoints

--- src/index.ts
+++ src/index.ts
@@ -19,6 +19,11 @@ app.get("/hello", (req, res) => {
 app.get("/products", async (req, res) => {
   try {
     const products = await pool.query("SELECT * FROM product");
+
+    if (products.rowCount === 0) {
+      return res.status(404).json({ message: "No products found" });
+    }
+
     res.status(200).json(products.rows);
   } catch (error) {
     console.log(error);
@@ -34,6 +39,11 @@ app.get("/products/:id", async (req, res) => {
       "SELECT * FROM product WHERE product_id = $1",
       [id]
     );
+
+    if (product.rows.length === 0) {
+      return res.status(404).json({ error: "Product not found" });
+    }
+
     res.status(200).json(product.rows[0]);
   } catch (error) {
     console.log(error);
@@ -50,6 +60,11 @@ app.post("/product/create", async (req, res) => {
       "INSERT INTO product (title, description, categories, quantity, price) VALUES ($1, $2, $3, $4, $5) RETURNING *",
       [title, description, categories, quantity, price]
     );
+
+    if (product.rowCount === 0) {
+      return res.status(500).json({ error: "Failed to create product" });
+    }
+
     res.status(200).json(product.rows[0]);
   } catch (error) {
     console.log(error);
@@ -67,6 +82,11 @@ app.put("/product/update/:id", async (req, res) => {
       "UPDATE product SET title = $1, description = $2, categories = $3, quantity = $4, price = $5 WHERE product_id = $6 RETURNING *",
       [title, description, categories, quantity, price, id]
     );
+
+    if (updateProduct.rowCount === 0) {
+      return res.status(404).json({ error: "Product doesn't exist" });
+    }
+
     res.status(200).json(updateProduct.rows[0]);
   } catch (error) {
     console.log(error);
@@ -82,6 +102,11 @@ app.delete("/product/delete/:id", async (req, res) => {
       "DELETE FROM product WHERE product_id = $1",
       [id]
     );
+
+    if (deleteProduct.rowCount === 0) {
+      return res.status(404).json({ error: "Product doesn't exist" });
+    }
+
     res.status(200).json("Deleted Sucessfully!");
   } catch (error) {
     console.log(error);
