403c15828b244cc7abe93caa8eb6be69f6a2c93e
EkhfaHossain <ekhfa.hossain@sazim.io>
2023-11-17 17:41:35 +0600

    Created Product Routes

---
+++ src/routes/productRoutes.ts
@@ -0,0 +1,27 @@
+import express, { Router } from "express";
+import {
+  getAllProducts,
+  getSingleProduct,
+  createProduct,
+  updateProduct,
+  deleteProduct,
+} from "../controller/productsController";
+
+const router: Router = express.Router();
+
+// For Getting all the Products
+router.get("/products", getAllProducts);
+
+// For Getting Single Product
+router.get("/products/:id", getSingleProduct);
+
+// For Creating Product
+router.post("/product/create", createProduct);
+
+// For Updating Product
+router.put("/product/update/:id", updateProduct);
+
+// For Deleting Product
+router.delete("/product/delete/:id", deleteProduct);
+
+export default router;
