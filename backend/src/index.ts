6872cfda013340fb40090a8132e4f8842eac518d
EkhfaHossain <ekhfa.hossain@sazim.io>
2023-11-20 21:58:03 +0600

    Added Cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,17 @@
 import express from "express";
+import cors from "cors";
 import productRoutes from "./routes/productRoutes";
 
 const app = express();
 
 app.use(express.json());
+app.use(
+  cors({
+    origin: ["http://localhost:3000", "http://localhost:9090"],
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    credentials: true,
+  })
+);
 
 app.use("/", productRoutes);
 
