19d0ee0b98ae9af2e177821d4100227fbac224ef
EkhfaHossain <ekhfa.hossain@sazim.io>
2023-11-21 14:10:03 +0600

    Changed type in API

--- backend/src/controller/productsController.ts
+++ backend/src/controller/productsController.ts
@@ -1,4 +1,4 @@
-import { Request, Response } from "express";
+import { type Request, type Response } from "express";
 import pool from "../configs/db";
 
 export const testRoute = (req: Request, res: Response) => {
