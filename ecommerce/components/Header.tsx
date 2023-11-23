8af7042d142d5d91c72facc5c3aef75acdd310d1
EkhfaHossain <ekhfa.hossain@sazim.io>
2023-11-23 14:26:55 +0600

    Made the Ecommerce title Clickable

--- ecommerce/components/Header.tsx
+++ ecommerce/components/Header.tsx
@@ -1,19 +1,22 @@
 import React from "react";
+import Link from "next/link";
 
 const Header = () => {
   return (
-    <header className="bg-lightBlue py-5 shadow-md">
+    <header className="bg-navColor py-5 shadow-lg">
       <div className="container max-w-screen-xl mx-auto px-4">
         <div className="flex flex-wrap items-center">
-          <div className="px-6 text-2xl font-mono text-gray-700">Ecommerce</div>
+          <Link href="/" passHref>
+            <div className="px-2 text-2xl font-mono text-white">Ecommerce</div>
+          </Link>
           <div className="flex items-center space-x-4 ml-auto">
             <div className="inline-block bg-white shadow-md hover:shadow-lg border border-borderColor rounded-md">
-              <button className="px-4 py-2 text-gray-700 hover:bg-borderColor hover:border-borderColor">
+              <button className="px-4 py-2 text-gray-700 font-semibold hover:bg-borderColor hover:border-borderColor">
                 Add Product
               </button>
             </div>
             <div className="inline-block bg-white shadow-md hover:shadow-lg border border-borderColor rounded-md">
-              <button className="px-4 py-2 text-gray-700 hover:bg-borderColor hover:border-borderColor">
+              <button className="px-4 py-2 text-gray-700 font-semibold hover:bg-borderColor hover:border-borderColor">
                 Sign In
               </button>
             </div>
