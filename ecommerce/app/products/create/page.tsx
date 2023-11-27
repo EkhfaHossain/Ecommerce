f745862ea5d462c766789560ddf251d81b69f911
EkhfaHossain <ekhfa.hossain@sazim.io>
2023-11-27 17:02:11 +0600

    Implemented Product Create Form

---
+++ ecommerce/app/products/create/page.tsx
@@ -0,0 +1,178 @@
+"use client";
+import React, { useState, ChangeEvent } from "react";
+import axios from "axios";
+
+interface Product {
+  title: string;
+  description: string;
+  categories: string;
+  price: number;
+  quantity: number;
+}
+
+const Form: React.FC = () => {
+  const [formData, setFormData] = useState<Product>({
+    title: "",
+    price: 0,
+    categories: "",
+    quantity: 0,
+    description: "",
+  });
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log("formDataTesting", formData);
+
+    try {
+      const response = await axios.post(
+        "http://localhost:9090/product/create",
+        formData
+      );
+      console.log("Product submitted:", response.data);
+      setFormData({
+        title: "",
+        price: 0,
+        categories: "",
+        quantity: 0,
+        description: "",
+      });
+    } catch (error) {
+      console.error("An error occurred", error);
+    }
+  };
+
+  const handleInputChange = (
+    e: ChangeEvent<HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement>
+  ) => {
+    const { name, value } = e.target;
+    console.log("Input Changed - Name:", name, "Value:", value);
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      [name]: name === "item-quantity" ? String(value) : value,
+    }));
+  };
+
+  const handleCategoryChange = (e: ChangeEvent<HTMLSelectElement>) => {
+    const { value } = e.target;
+    console.log("Category Changed:", value);
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      categories: value,
+    }));
+  };
+
+  return (
+    <section className="bg-white dark:bg-gray-900">
+      <div className="py-8 px-4 mx-auto max-w-2xl lg:py-16">
+        <h2 className="mb-4 text-xl font-bold text-gray-900 dark:text-white">
+          Add a new product
+        </h2>
+        <form onSubmit={handleSubmit}>
+          <div className="grid gap-4 sm:grid-cols-2 sm:gap-6">
+            <div className="sm:col-span-2">
+              <label
+                htmlFor="title"
+                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+              >
+                Product Name
+              </label>
+              <input
+                type="text"
+                name="title"
+                id="title"
+                value={formData.title}
+                onChange={handleInputChange}
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                placeholder="Type product name"
+                required
+              />
+            </div>
+
+            <div className="w-full">
+              <label
+                htmlFor="price"
+                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+              >
+                Price
+              </label>
+              <input
+                type="number"
+                name="price"
+                id="price"
+                value={formData.price}
+                onChange={handleInputChange}
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                placeholder="$2999"
+                required
+              />
+            </div>
+            <div>
+              <label
+                htmlFor="categories"
+                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+              >
+                Category
+              </label>
+              <select
+                id="categories"
+                value={formData.categories}
+                onChange={handleCategoryChange}
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 sm:w-full"
+              >
+                <option disabled hidden>
+                  Select category
+                </option>
+                <option value="GROCERIES">Groceries</option>
+                <option value="SPORTS">Sports</option>
+                <option value="SNACKS">Snacks</option>
+              </select>
+            </div>
+            <div>
+              <label
+                htmlFor="quantity"
+                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+              >
+                Quantity
+              </label>
+              <input
+                type="number"
+                name="quantity"
+                value={formData.quantity}
+                onChange={handleInputChange}
+                id="quantity"
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                placeholder="12"
+                required
+              />
+            </div>
+            <div className="sm:col-span-2">
+              <label
+                htmlFor="description"
+                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+              >
+                Description
+              </label>
+              <textarea
+                name="description"
+                id="description"
+                value={formData.description}
+                onChange={handleInputChange}
+                rows={8}
+                className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                placeholder="Your description here"
+              ></textarea>
+            </div>
+          </div>
+          <button
+            type="submit"
+            className="inline-flex items-center px-5 py-2.5 mt-4 sm:mt-6 text-sm font-medium text-center text-white bg-primary-700 rounded-lg focus:ring-4 focus:ring-primary-200 dark:focus:ring-primary-900 hover:bg-primary-800"
+          >
+            Submit
+          </button>
+        </form>
+      </div>
+    </section>
+  );
+};
+
+export default Form;
