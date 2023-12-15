5853c5c6b0e4de45649cd8b850a59eb7bb4b7eb5
EkhfaHossain <ekhfa.hossain@sazim.io>
2023-12-15 16:50:21 +0600

    Added Toast

--- ecommerce/app/layout.tsx
+++ ecommerce/app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next";
 // These styles apply to every route in the application
 import "./globals.css";
 import Header from "@/components/Header";
+import { Toaster } from "react-hot-toast";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -17,6 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
+        <Toaster position="top-center" />
         <Header />
         {children}
       </body>
