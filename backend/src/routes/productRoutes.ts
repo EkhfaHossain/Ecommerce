3e890c9a90531f0fff9c84e3104012132bcf9872
EkhfaHossain <ekhfa.hossain@sazim.io>
2023-12-25 21:19:56 +0600

    Added Middleware to the routes that needs middleware

--- backend/src/routes/productRoutes.ts
+++ backend/src/routes/productRoutes.ts
@@ -8,24 +8,26 @@ import {
   testRoute,
 } from "../controller/productsController";
 
+import { authenticateAndAuthorizeMiddleware } from "../middleware/authMiddleWare";
+
 const router: Router = express.Router();
 
-// Test Route
-router.get("/", testRoute);
-
-// For Getting all the Products
 router.get("/products", getAllProducts);
-
-// For Getting Single Product
 router.get("/products/:id", getSingleProduct);
-
-// For Creating Product
-router.post("/product/create", createProduct);
-
-// For Updating Product
-router.put("/product/update/:id", updateProduct);
-
-// For Deleting Product
-router.delete("/product/delete/:id", deleteProduct);
+router.post(
+  "/product/create",
+  authenticateAndAuthorizeMiddleware,
+  createProduct
+);
+router.put(
+  "/product/update/:id",
+  authenticateAndAuthorizeMiddleware,
+  updateProduct
+);
+router.delete(
+  "/product/delete/:id",
+  authenticateAndAuthorizeMiddleware,
+  deleteProduct
+);
 
 export default router;
