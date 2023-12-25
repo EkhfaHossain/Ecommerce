bf8e47dd0365f6328b6413fdde8c50c50c818d32
EkhfaHossain <ekhfa.hossain@sazim.io>
2023-12-25 21:18:32 +0600

    Added Cookie Parser Middleware

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,19 @@
 import express from "express";
 import cors from "cors";
+import cookieParser from "cookie-parser";
 import productRoutes from "./routes/productRoutes";
 import userRoutes from "./routes/userRoutes";
+import { authenticateAndAuthorizeMiddleware } from "./middleware/authMiddleWare";
 
 const app = express();
-
+app.use(cookieParser());
 app.use(express.json());
+
+// app.use((req, res, next) => {
+//   console.log("Cookies:", req.cookies);
+//   next();
+// });
+
 app.use(
   cors({
     origin: ["http://localhost:3000", "http://localhost:9090"],
@@ -16,8 +24,9 @@ app.use(
 // serve static files from public folder
 app.use(express.static("public"));
 app.use("/images", express.static("images"));
-app.use("/", userRoutes);
+
 app.use("/", productRoutes);
+app.use("/", userRoutes);
 
 const PORT = process.env.PORT || 9090;
 app.listen(PORT, () => {
