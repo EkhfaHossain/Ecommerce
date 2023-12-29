362aa84587cbabf80e3430249ce9eb3ac1399fbf
EkhfaHossain <ekhfa.hossain@sazim.io>
2023-12-29 19:43:34 +0600

    Added Status button

--- ecommerce/app/admin/order-dashboard/page.tsx
+++ ecommerce/app/admin/order-dashboard/page.tsx
@@ -6,6 +6,10 @@ import axios from "axios";
 const PurchasesPage = () => {
   const [purchases, setPurchases] = useState<any[]>([]);
 
+  const handleStatusChange = (id: number) => {
+    console.log("changed the status");
+  };
+
   useEffect(() => {
     const fetchData = async () => {
       try {
@@ -24,27 +28,44 @@ const PurchasesPage = () => {
   return (
     <div className="container mx-auto px-4 py-8">
       <h1 className="text-2xl font-bold mb-4 text-center">All Purchases</h1>
-      <div className="grid grid-cols-2 gap-4">
+      <div>
         {purchases.map((purchase, index) => (
-          <div key={index} className="border p-4 rounded shadow">
-            <h2 className="text-lg font-semibold mb-2">Customer</h2>
-            <p>
-              <strong>ID:</strong> {purchase.customer.id}
-            </p>
-            <p>
-              <strong>Name:</strong> {purchase.customer.name}
-            </p>
-            <p>
-              <strong>Email:</strong> {purchase.customer.email}
-            </p>
+          <div key={index} className="border p-4 rounded shadow mb-4">
+            <div className="mb-4">
+              <h2 className="text-lg font-semibold mb-2">Customer</h2>
+              <p>
+                <strong>ID:</strong> {purchase.customer.id}
+              </p>
+              <p>
+                <strong>Name:</strong> {purchase.customer.name}
+              </p>
+              <p>
+                <strong>Email:</strong> {purchase.customer.email}
+              </p>
+            </div>
             <hr className="my-4" />
-            <h2 className="text-lg font-semibold mb-2">Product</h2>
-            <p>
-              <strong>ID:</strong> {purchase.product.id}
-            </p>
-            <p>
-              <strong>Name:</strong> {purchase.product.name}
-            </p>
+            <div>
+              <h2 className="text-lg font-semibold mb-2">Products Bought</h2>
+              {purchase.products.map((product: any, productIndex: number) => (
+                <div key={productIndex} className="mb-2">
+                  <p>
+                    <strong>ID:</strong> {product.id || "N/A"}
+                  </p>
+                  <p>
+                    <strong>Title:</strong> {product.title || "N/A"}
+                  </p>
+                  <p className="status-container">
+                    <strong>Status:</strong> {product.status}{" "}
+                    <button
+                      className="bg-blue-500 text-white px-3 py-1 rounded-md ml-4"
+                      onClick={() => handleStatusChange(product.id)}
+                    >
+                      Change Status
+                    </button>
+                  </p>
+                </div>
+              ))}
+            </div>
           </div>
         ))}
       </div>
