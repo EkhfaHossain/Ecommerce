76a24db2cba1d6265a14701a03220c9fc3ca61ef
EkhfaHossain <ekhfa.hossain@sazim.io>
2023-12-29 14:54:56 +0600

    Implemented Category Filter to the All Products Page

--- ecommerce/app/products/page.tsx
+++ ecommerce/app/products/page.tsx
@@ -5,6 +5,8 @@ import Link from "next/link";
 import Card from "@/components/Card";
 import CategoryFilter from "@/components/CategoryFilter";
 import Pagination from "@/components/Pagination";
+import PriceFilter from "@/components/PriceFilter";
+import { useRouter } from "next/navigation";
 
 interface Product {
   id: number;
@@ -17,35 +19,83 @@ interface Product {
 }
 
 const ListProducts = () => {
+  const router = useRouter();
   const [products, setProducts] = useState<Product[]>([]);
   const [currentPage, setCurrentPage] = useState(1);
   const [totalPages, setTotalPages] = useState(1);
+  const [isLoading, setIsLoading] = useState(true);
+  const [minPrice, setMinPrice] = useState<number | "">("");
+  const [maxPrice, setMaxPrice] = useState<number | "">("");
+  const [selectedCategory, setSelectedCategory] = useState<string | null>(null);
 
-  useEffect(() => {
-    const fetchProducts = async (page: any) => {
-      try {
-        const response = await axios.get(
-          `http://localhost:9090/products?page=${page}`
-        );
-        setProducts(response.data.products);
-        setTotalPages(response.data.totalPages);
-        setCurrentPage(page);
-      } catch (error) {
-        console.error("Error fetching products:", error);
-      }
-    };
-
-    fetchProducts(currentPage);
-  }, [currentPage]);
+  const predefinedCategories = [
+    "Electronics",
+    "Snacks",
+    "Groceries",
+    "Makeup",
+    "Sports",
+  ];
 
   const handlePageChange = (page: any) => {
     setCurrentPage(page);
   };
 
+  const handleFilterChange = ({ min, max }: { min: number; max: number }) => {
+    setMinPrice(min);
+    setMaxPrice(max);
+    setCurrentPage(1);
+  };
+
+  const handleClearFilters = () => {
+    setMinPrice("");
+    setMaxPrice("");
+    setCurrentPage(1);
+  };
+
+  const handleCategoryChange = (category: string | null) => {
+    setSelectedCategory(category);
+    setCurrentPage(1);
+  };
+
+  useEffect(() => {
+    const fetchProducts = async (page: any) => {
+      setIsLoading(true);
+      try {
+        let url = `http://localhost:9090/products?page=${page}`;
+        if (minPrice !== "" && maxPrice !== "") {
+          url += `&min=${minPrice}&max=${maxPrice}`;
+        }
+        if (selectedCategory) {
+          url += `&category=${selectedCategory}`;
+        }
+        console.log(url);
+        const response = await axios.get(url);
+        setProducts(response.data.products);
+        setTotalPages(response.data.totalPages);
+        setCurrentPage(page);
+      } catch (error) {
+        console.error("Error fetching products:", error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchProducts(currentPage);
+  }, [currentPage, minPrice, maxPrice, selectedCategory]);
+
   return (
-    <section className="py-12">
-      {/* <CategoryFilter /> */}
-      <div className="max-w-screen-xl container mx-auto px-4">
+    <section className="py-4">
+      <CategoryFilter
+        categories={predefinedCategories}
+        onCategoryChange={handleCategoryChange}
+      />
+
+      <PriceFilter
+        onFilterChange={handleFilterChange}
+        onFilterClear={handleClearFilters}
+      />
+
+      <div className="max-w-screen-xl container mx-auto px-4 mt-8">
         <div className="md:flex-row -mx-4 flex flex-wrap">
           {products.map((product) => (
             <Card key={product.id}>
@@ -85,12 +135,14 @@ const ListProducts = () => {
           ))}
         </div>
       </div>
-      <div className="flex justify-center items-center mb-10">
-        <Pagination
-          totalPages={totalPages}
-          currentPage={currentPage}
-          onPageChange={handlePageChange}
-        />
+      <div className="flex justify-center items-center mt-8">
+        {!isLoading && (
+          <Pagination
+            totalPages={totalPages}
+            currentPage={currentPage}
+            onPageChange={handlePageChange}
+          />
+        )}
       </div>
     </section>
   );
